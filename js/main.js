c12d437156de81620765cb5c4a0c6537bab26c25
Alex Mori <almoriex@gmail.com>
2017-05-23 10:07:01 -0700

    added a badge beside cart
    Signed-off-by: Alex Mori <almoriex@gmail.com>

--- js/main.js
+++ js/main.js
@@ -12,7 +12,8 @@ $(document).ready(function() {
     var holderDiv = document.getElementById("holder");
     var warningDiv = document.getElementById("warning");
     var checkoutButton = document.getElementById("checkout");
-    var shopStatusDiv = document.getElementById("closed")
+    var shopStatusDiv = document.getElementById("closed");
+    var badge = document.getElementById("badge");
     
     document.addEventListener("scroll", function() {
         warningDiv.style.display = "none";
@@ -157,6 +158,7 @@ $(document).ready(function() {
                 total_price = total_price + (parseInt(itemQty.value) * parseFloat(price));
 
                 ordersCount++;
+                badge.innerHTML = ordersCount;
             } else if (cartItem != null) {
                 warningDiv.style.display = "inline";
                 warningDiv.innerHTML = "You already have this item in the cart.";
@@ -247,6 +249,7 @@ $(document).ready(function() {
             delete orders[item_code];
 
             ordersCount--;
+            badge.innerHTML = ordersCount;
         });
     }
     
